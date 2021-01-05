51f81fe0eca095f4e98b30e80fb04f6bf9d3c435
Godwill <goddhigoddhi@gmail.com>
2021-01-05 16:59:57 +0100

    git

---
+++ src/requests.js
@@ -0,0 +1,15 @@
+const API_KEY = "3b8056c67e883faf6e836ede26eaad02"
+
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&langusage=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&langusage=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomaancMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+}
+
+export default requests
+  
